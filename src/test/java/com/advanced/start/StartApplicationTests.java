7d465e6836f1cb344f1f8c11a4f9a50290156080
twocowsong <woosong0215@naver.com>
2022-05-21 17:19:40 +0900

    최초 커밋

---
+++ src/test/java/com/advanced/start/StartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advanced.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
